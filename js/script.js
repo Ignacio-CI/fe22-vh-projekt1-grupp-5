d77022e5d9fe27693ad0a4716d47415b8a68e83b
RobinGrit22 <bognar.robin@hotmail.com>
2023-01-16 17:33:04 +0100

    Added jquerry link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-$(() => {
-
-    $('.nav-links').click((e) => {
-        console.log("test");
-    });
-
-});
+$('#btn').click(function(event){
+    event.preventDefault();
+    let cityInput = $('#city-input').val();
+    let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=6dfc3427f390e2925376be035ae60bdf`
+    $.getJSON(apiUrl,{ 
+    }).done(function(data){
+        console.log(data)
+    }
+    ) 
+})
