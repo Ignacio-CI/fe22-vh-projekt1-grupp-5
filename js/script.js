b0e7096d67ec0661a203b33473e6272b301f9b30
vijoh01 <vijoh01@gmail.com>
2023-01-17 16:42:59 +0100

    Kopplat api till ui på homepage

--- js/script.js
+++ js/script.js
@@ -9,6 +9,19 @@ $(()=>{
             console.log(data);
         }) 
     })
+
+    let apiUrl = `http://api.openweathermap.org/data/2.5/group?id=2673730,3169070,2643743,1850144&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
+        $.getJSON(apiUrl, { 
+        }).done((data) => {
+            let country = data.list;
+            console.log(country);
+            country.forEach((element, index) => {
+                $(`#card${index} .headers h2`).text(element.name);
+                $(`#card${index} img`).attr('src',`http://openweathermap.org/img/wn/${element.weather[0].icon}@4x.png`);
+                $(`#card${index} .headers p`).text(element.weather[0].main);
+                $(`#card${index} h3`).text(Math.round(element.main.temp) + " °C");
+            });
+        }) 
 })
 
 const toggleButton = document.querySelector('#toggle-button');
