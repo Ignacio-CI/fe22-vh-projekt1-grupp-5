6cb7243683bbe03f372de8400eaa030ecdd3ca2a
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2023-01-18 09:54:50 +0100

    weather app functions

--- js/script.js
+++ js/script.js
@@ -2,19 +2,32 @@ $(()=>{
     $('#weather-app-btn').click(function(event){
         event.preventDefault();
         let cityInput = $('#city-input').val();
-
+    
+        if(!cityInput) {
+          alert("Please enter a city name.");
+          return;
+        }
+    
         let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
         $.getJSON(apiUrl, { 
         }).done((data) => {
-            console.log(data);
-        }) 
-    })
+            console.log(data)
+            $('#card0 h2').text(data.name)
+            $(`#card0 img`).attr('src',`http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`);
+            $(`#card0 .headers p`).text(data.weather[0].main);
+            $(`#card0 h3`).text(Math.round(data.main.temp) + "°")
+        }).fail((jqXHR, textStatus, errorThrown) => {
+            console.log(jqXHR, textStatus, errorThrown)
+            alert("City not found. Please enter a valid city name.")
+            return
+        });
+    });
 
     let apiUrl = `http://api.openweathermap.org/data/2.5/group?id=2673730,3169070,2643743,1850144&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
         $.getJSON(apiUrl, { 
         }).done((data) => {
             let country = data.list;
-            console.log(country);
+            //  console.log(data);
             country.forEach((element, index) => {
                 $(`#card${index} .headers h2`).text(element.name);
                 $(`#card${index} img`).attr('src',`http://openweathermap.org/img/wn/${element.weather[0].icon}@4x.png`);
