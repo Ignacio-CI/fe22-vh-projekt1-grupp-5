cec565da2b0e9cb30f3568560936ba012c375b2c
vijoh01 <vijoh01@gmail.com>
2023-01-18 21:48:33 +0100

    slider återställs nu vid ny sökning

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(()=>{
           alert("Please enter a city name.");
           return;
         }
-    
+        $( ".slider" ).val(0);
         let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
         $.getJSON(apiUrl, { 
         }).done((data) => {
