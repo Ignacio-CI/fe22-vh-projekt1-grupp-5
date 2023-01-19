4cec4e8964db1dcda50ab2c33e9d1c3216c99583
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2023-01-19 22:26:13 +0100

    https change

--- js/script.js
+++ js/script.js
@@ -55,14 +55,14 @@ $(()=>{
     });
     
 
-    let apiUrl = `http://api.openweathermap.org/data/2.5/group?id=2673730,3169070,2643743,1850144&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/group?id=2673730,3169070,2643743,1850144&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
         $.getJSON(apiUrl, { 
         }).done((data) => {
             let country = data.list;
             console.log(data);
             country.forEach((element, index) => {
                 $(`#card${index} .headers h2`).text(element.name);
-                $(`#card${index} img`).attr('src',`http://openweathermap.org/img/wn/${element.weather[0].icon}@4x.png`);
+                $(`#card${index} img`).attr('src',`https://openweathermap.org/img/wn/${element.weather[0].icon}@4x.png`);
                 $(`#card${index} .headers p`).text(element.weather[0].main);
                 $(`#card${index} h3`).text(Math.round(element.main.temp) + "°");
                 $(`#card${index} #feels-like`).text(Math.round(element.main.feels_like) + "°");
