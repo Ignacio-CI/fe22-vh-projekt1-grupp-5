238a9b97d8f095f4af6851b29d7dbbd9bddf5107
vijoh01 <vijoh01@gmail.com>
2023-01-19 23:05:25 +0100

    Uppdaterat slidern

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(()=>{
-
+//https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05
     $('#weather-app-btn').click(function(event){
         event.preventDefault();
         let cityInput = $('#city-input').val();
@@ -9,18 +9,25 @@ $(()=>{
           return;
         }
         $( ".slider" ).val(0);
-        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
-        $.getJSON(apiUrl, { 
+        let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&units=metric&cnt=16&appid=8de34fe193f5396bc8ece49b21d39f05`
+        $.getJSON(apiUrl, {
         }).done((data) => {
             console.log(data);
-            $('#card0 h2').text(data.name)
-            $(`#card0 img`).attr('src',`http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`);
-            $(`#card0 .headers p`).text(data.weather[0].main);
-            $(`#card0 h3`).text(Math.round(data.main.temp) + "°");
-            $('#card0 #feels-like').text(Math.round(data.main.feels_like) + "°");
-            $('#card0 #temp-max').text(Math.round(data.main.temp_min) + "°");
-            $('#card0 #temp-min').text(Math.round(data.main.temp_max) + "°");
-            $('#card0 #humidity').text(Math.round(data.main.humidity) + "%");
+            $('#card0 h2').text(data.city.name)
+            $(`#card0 img`).attr('src',`https://openweathermap.org/img/wn/${data.list[$('.slider').val()].weather[0].icon}@4x.png`);
+            $(`#card0 .headers p`).text(data.list[$('.slider').val()].weather[0].main);
+            $(`#card0 h3`).text(Math.round(data.list[$('.slider').val()].main.temp) + "°");
+            $('#card0 #feels-like').text(Math.round(data.list[$('.slider').val()].main.feels_like) + "°");
+            $('#card0 #temp-max').text(Math.round(data.list[$('.slider').val()].main.temp_min) + "°");
+            $('#card0 #temp-min').text(Math.round(data.list[$('.slider').val()].main.temp_max) + "°");
+            $('#card0 #humidity').text(Math.round(data.list[$('.slider').val()].main.humidity) + "%");
+
+            const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        
+            const d = new Date(data.list[$('.slider').val()].dt_txt);
+            let day = weekday[d.getDay()];
+
+            $('#time').text(day + " " + d.getHours() + ":00");
         }).fail((jqXHR, textStatus, errorThrown) => {
             // console.log(jqXHR, textStatus, errorThrown)
             alert("City not found. Please enter a valid city name.")
@@ -29,12 +36,12 @@ $(()=>{
     });
 
     $( ".slider" ).change(function() {
-        let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${$('#card0 h2').text()}&units=metric&cnt=16&appid=8de34fe193f5396bc8ece49b21d39f05`
+        let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${$('#card0 h2').text()}&units=metric&cnt=16&appid=8de34fe193f5396bc8ece49b21d39f05`
         $.getJSON(apiUrl, { 
         }).done((data) => {
             console.log(data);
             $('#card0 h2').text(data.city.name)
-            $(`#card0 img`).attr('src',`http://openweathermap.org/img/wn/${data.list[$('.slider').val()].weather[0].icon}@4x.png`);
+            $(`#card0 img`).attr('src',`https://openweathermap.org/img/wn/${data.list[$('.slider').val()].weather[0].icon}@4x.png`);
             $(`#card0 .headers p`).text(data.list[$('.slider').val()].weather[0].main);
             $(`#card0 h3`).text(Math.round(data.list[$('.slider').val()].main.temp) + "°");
             $('#card0 #feels-like').text(Math.round(data.list[$('.slider').val()].main.feels_like) + "°");
@@ -53,16 +60,15 @@ $(()=>{
             return
         });
     });
-    
 
-    let apiUrl = `http://api.openweathermap.org/data/2.5/group?id=2673730,3169070,2643743,1850144&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/group?id=2673730,3169070,2643743,1850144&units=metric&appid=8de34fe193f5396bc8ece49b21d39f05`
         $.getJSON(apiUrl, { 
         }).done((data) => {
             let country = data.list;
             console.log(data);
             country.forEach((element, index) => {
                 $(`#card${index} .headers h2`).text(element.name);
-                $(`#card${index} img`).attr('src',`http://openweathermap.org/img/wn/${element.weather[0].icon}@4x.png`);
+                $(`#card${index} img`).attr('src',`https://openweathermap.org/img/wn/${element.weather[0].icon}@4x.png`);
                 $(`#card${index} .headers p`).text(element.weather[0].main);
                 $(`#card${index} h3`).text(Math.round(element.main.temp) + "°");
                 $(`#card${index} #feels-like`).text(Math.round(element.main.feels_like) + "°");
